77e1e28a322222d04bc442a077f192acc35c7b66
priyankaugile <ugilepriyanka26@gmail.com>
2021-09-02 19:37:29 +0530

    Added: code of Stock Account Management.

---
+++ stockaccountmanagement/StockDetailsMain.java
@@ -0,0 +1,27 @@
+package com.stockaccountmanagement;
+
+public class StockDetailsMain {
+    public static void main(String[] args) {
+        StockDetailsService stockDetailsService = new StockDetailsService();
+
+        int choice = 0;
+        int exitValue = 4;
+        while (choice != exitValue){
+            StockDetailsModel.displayOptions();
+            choice = StockDetailsModel.readInteger();
+            switch (choice){
+                case 1:
+                    stockDetailsService.addStock();
+                    break;
+                case 2:
+                    stockDetailsService.getStockDetails();
+                    break;
+
+                case 3:
+                    stockDetailsService.displayRecord();
+                    break;
+
+            }
+        }
+    }
+}
